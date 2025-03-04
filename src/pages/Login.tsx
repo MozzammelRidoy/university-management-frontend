f8851a6830d84c97c69c15d41b326cdd5c859127
MozzammelRidoy <mozzammelridoy5iii@gmail.com>
2025-03-05 05:36:50 +0600

    create basic login form and set hook form and loginMutation

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,44 @@
+import { Button } from "antd";
+import { useForm } from "react-hook-form";
+import { useLoginMutation } from "../redux/features/auth/authApi";
+
 const Login = () => {
+  const { handleSubmit, register } = useForm({
+    defaultValues: {
+      id: "A-0001",
+      password: "admin123",
+    },
+  });
+
+  const [login, { data, error }] = useLoginMutation();
+
+  console.log("data=> ", data);
+  console.log("error=> ", error);
+
+  const onSubmit = (data: any) => {
+    const userInfo = {
+      id: data.id,
+      password: data.password,
+    };
+    login(userInfo);
+    console.log(userInfo);
+  };
   return (
     <div>
-      <h2>This is Login page</h2>
+      <h2>Login here</h2>
+
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div>
+          <label htmlFor="id">ID : </label>
+          <input type="text" id="id" {...register("id")} />
+        </div>
+        <div>
+          <label htmlFor="password">Password : </label>
+          <input type="text" id="password" {...register("password")} />
+        </div>
+
+        <Button htmlType="submit">Login</Button>
+      </form>
     </div>
   );
 };
