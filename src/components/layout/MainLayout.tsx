48a26445ba1b2da65c2498a97f0530c7b8ad664f
MozzammelRidoy <mozzammelridoy5iii@gmail.com>
2025-03-05 07:03:04 +0600

    set sonner and user logout method

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,44 +1,21 @@
-import { Layout } from "antd";
+import { Button, Layout } from "antd";
 import { Content, Header } from "antd/es/layout/layout";
 
 import { Outlet } from "react-router-dom";
 import Sidebar from "./Sidebar";
-
-// const items: MenuProps["items"] = [
-//   {
-//     key: "Dashboard",
-//     label: <NavLink to={"/admin/dashboard"}>Dashboard</NavLink>,
-//   },
-//   {
-//     key: "Profile",
-//     label: "Profile",
-//   },
-//   {
-//     key: "User Management",
-//     label: "User Management",
-//     children: [
-//       {
-//         key: "Create Admin",
-//         label: <NavLink to={"/admin/create-admin"}>Create Admin</NavLink>,
-//       },
-//       {
-//         key: "Create Faculty",
-//         label: <NavLink to={"/admin/create-faculty"}>Create Faculty</NavLink>,
-//       },
-//       {
-//         key: "Create Student",
-//         label: <NavLink to={"/admin/create-student"}>Create Student</NavLink>,
-//       },
-//     ],
-//   },
-// ];
+import { useAppDispatch } from "../../redux/hooks";
+import { logout } from "../../redux/features/auth/authSlice";
 
 const MainLayout = () => {
+  const dispatch = useAppDispatch();
+
   return (
     <Layout style={{ minHeight: "100vh" }}>
       <Sidebar />
       <Layout>
-        <Header style={{ padding: 0 }} />
+        <Header style={{ padding: 0 }}>
+          <Button onClick={() => dispatch(logout())}>Log out</Button>
+        </Header>
         <Content style={{ margin: "24px 16px 0" }}>
           <div
             style={{
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,10 +1,11 @@
 import { Button } from "antd";
-import { useForm } from "react-hook-form";
+import { FieldValues, useForm } from "react-hook-form";
 import { useLoginMutation } from "../redux/features/auth/authApi";
 import { useAppDispatch } from "../redux/hooks";
-import { setUser } from "../redux/features/auth/authSlice";
+import { setUser, TUser } from "../redux/features/auth/authSlice";
 import { verifyToken } from "../utils/verifyToken";
 import { useNavigate } from "react-router-dom";
+import { toast } from "sonner";
 
 const Login = () => {
   const dispatch = useAppDispatch();
@@ -17,17 +18,24 @@ const Login = () => {
     },
   });
 
-  const [login, { error }] = useLoginMutation();
+  const [login] = useLoginMutation();
 
-  const onSubmit = async (data: any) => {
-    const userInfo = {
-      id: data.id,
-      password: data.password,
-    };
-    const res = await login(userInfo).unwrap();
-    const user = verifyToken(res.data.accessToken);
-    dispatch(setUser({ user, token: res.data.accessToken }));
-    navigate("/admin/dashboard");
+  const onSubmit = async (data: FieldValues) => {
+    const toastId = toast.loading("Logging in...");
+
+    try {
+      const userInfo = {
+        id: data.id,
+        password: data.password,
+      };
+      const res = await login(userInfo).unwrap();
+      const user = verifyToken(res.data.accessToken) as TUser;
+      dispatch(setUser({ user, token: res.data.accessToken }));
+      navigate(`/${user.role}/dashboard`);
+      toast.success("Logged in successfully!", { id: toastId, duration: 1000 });
+    } catch (err) {
+      toast.error("Something went wrong!", { id: toastId, duration: 1000 });
+    }
   };
   return (
     <div>
