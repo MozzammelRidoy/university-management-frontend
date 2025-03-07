d95cabae20aea90023231494c2393b12655f06e1
MozzammelRidoy <mozzammelridoy5iii@gmail.com>
2025-03-07 12:44:06 +0600

    set up Academic management component

---
+++ src/pages/admin/academicManagement/CreateAcademicSemester.tsx
@@ -0,0 +1,20 @@
+import { FieldValues, SubmitHandler } from "react-hook-form";
+import PHForm from "../../../components/form/PHForm";
+import PHInput from "../../../components/form/PHInput";
+import { Button } from "antd";
+
+const CreateAcademicSemester = () => {
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    console.log(data);
+  };
+  return (
+    <div>
+      <PHForm onSubmit={onSubmit}>
+        <PHInput type="text" name="name" label="Semester Name" />
+        <Button htmlType="submit">Submit</Button>
+      </PHForm>
+    </div>
+  );
+};
+
+export default CreateAcademicSemester;
