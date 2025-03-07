ece48d253ef667fb0e98467ae666a50f6f913b3a
MozzammelRidoy <mozzammelridoy5iii@gmail.com>
2025-03-07 12:16:03 +0600

    fix type error and set defaultValues

--- src/components/form/PHForm.tsx
+++ src/components/form/PHForm.tsx
@@ -1,7 +1,25 @@
-import { FormProvider, useForm } from "react-hook-form";
+import { ReactNode } from "react";
+import {
+  FieldValues,
+  FormProvider,
+  SubmitHandler,
+  useForm,
+} from "react-hook-form";
 
-const PHForm = ({ onSubmit, children }) => {
-  const methods = useForm();
+type TFromConfig = {
+  defaultValues?: Record<string, any>;
+};
+type TFromPops = {
+  onSubmit: SubmitHandler<FieldValues>;
+  children: ReactNode;
+} & TFromConfig;
+
+const PHForm = ({ onSubmit, children, defaultValues }: TFromPops) => {
+  const formConfig: TFromConfig = {};
+  if (defaultValues) {
+    formConfig["defaultValues"] = defaultValues;
+  }
+  const methods = useForm(formConfig);
   return (
     <FormProvider {...methods}>
       <form onSubmit={methods.handleSubmit(onSubmit)}>{children}</form>;
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Button, Row } from "antd";
 import { FieldValues } from "react-hook-form";
 import { useLoginMutation } from "../redux/features/auth/authApi";
@@ -20,6 +21,10 @@ const Login = () => {
   //   },
   // });
 
+  const defaultValues = {
+    id: "A-0001",
+    password: "admin123",
+  };
   const [login] = useLoginMutation();
 
   const onSubmit = async (data: FieldValues) => {
@@ -43,7 +48,7 @@ const Login = () => {
   };
   return (
     <Row justify={"center"} align={"middle"} style={{ height: "100vh" }}>
-      <PHForm onSubmit={onSubmit}>
+      <PHForm onSubmit={onSubmit} defaultValues={defaultValues}>
         <PHInput type={"text"} name={"id"} label={"ID :"} />
 
         <PHInput type={"text"} name={"password"} label={"Password :"} />
