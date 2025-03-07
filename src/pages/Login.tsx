65be77d4601ae05ec944c5e29b13d36669dab7e5
MozzammelRidoy <mozzammelridoy5iii@gmail.com>
2025-03-07 08:13:48 +0600

    set type in baseQuery and create reusable form

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -6,6 +6,7 @@ import { setUser, TUser } from "../redux/features/auth/authSlice";
 import { verifyToken } from "../utils/verifyToken";
 import { useNavigate } from "react-router-dom";
 import { toast } from "sonner";
+import PHForm from "../components/form/PHForm";
 
 const Login = () => {
   const dispatch = useAppDispatch();
@@ -23,25 +24,27 @@ const Login = () => {
   const onSubmit = async (data: FieldValues) => {
     const toastId = toast.loading("Logging in...");
 
-    try {
-      const userInfo = {
-        id: data.id,
-        password: data.password,
-      };
-      const res = await login(userInfo).unwrap();
-      const user = verifyToken(res.data.accessToken) as TUser;
-      dispatch(setUser({ user, token: res.data.accessToken }));
-      navigate(`/${user.role}/dashboard`);
-      toast.success("Logged in successfully!", { id: toastId, duration: 1000 });
-    } catch (err) {
-      toast.error("Something went wrong!", { id: toastId, duration: 1000 });
-    }
+    console.log(data);
+
+    // try {
+    //   const userInfo = {
+    //     id: data.id,
+    //     password: data.password,
+    //   };
+    //   const res = await login(userInfo).unwrap();
+    //   const user = verifyToken(res.data.accessToken) as TUser;
+    //   dispatch(setUser({ user, token: res.data.accessToken }));
+    //   navigate(`/${user.role}/dashboard`);
+    //   toast.success("Logged in successfully!", { id: toastId, duration: 1000 });
+    // } catch (err) {
+    //   toast.error("Something went wrong!", { id: toastId, duration: 1000 });
+    // }
   };
   return (
     <div>
       <h2>Login here</h2>
 
-      <form onSubmit={handleSubmit(onSubmit)}>
+      <PHForm onSubmit={onSubmit}>
         <div>
           <label htmlFor="id">ID : </label>
           <input type="text" id="id" {...register("id")} />
@@ -52,7 +55,7 @@ const Login = () => {
         </div>
 
         <Button htmlType="submit">Login</Button>
-      </form>
+      </PHForm>
     </div>
   );
 };
