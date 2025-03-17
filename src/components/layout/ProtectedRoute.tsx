60c91a82352209ca1ad7562d25a96e6e206d67ed
MozzammelRidoy <mozzammelridoy5iii@gmail.com>
2025-03-17 07:15:53 +0600

    basic setup role base access contorl in protected route

--- src/components/layout/ProtectedRoute.tsx
+++ src/components/layout/ProtectedRoute.tsx
@@ -1,10 +1,23 @@
 import { ReactNode } from "react";
-import { useAppSelector } from "../../redux/hooks";
-import { selectCurrentToken } from "../../redux/features/auth/authSlice";
+import { useAppDispatch, useAppSelector } from "../../redux/hooks";
+import {
+  logout,
+  selectCurrentToken,
+  selectCurrentUser,
+} from "../../redux/features/auth/authSlice";
 import { Navigate } from "react-router-dom";
 
-const ProtectedRoute = ({ children }: { children: ReactNode }) => {
+type TProtectedRoute = { children: ReactNode; role: string | undefined };
+
+const ProtectedRoute = ({ children, role }: TProtectedRoute) => {
   const token = useAppSelector(selectCurrentToken);
+  const user = useAppSelector(selectCurrentUser);
+  const dispatch = useAppDispatch();
+
+  if (role !== undefined && role !== user?.role) {
+    dispatch(logout());
+    return <Navigate to={"/login"} replace={true} />;
+  }
 
   if (!token) {
     return <Navigate to={"/login"} replace={true} />;
